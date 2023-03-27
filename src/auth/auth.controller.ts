92c6b4423f6caa795fa1a0a3bcd991c6b99fe0b3
MrSharpp <amiralam@sofyrus.com>
2023-03-27 17:44:50 +0530

    swrc

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -16,13 +16,13 @@ import * as bcrypt from 'bcrypt';
 export class AuthController {
   constructor(
     private authServie: AuthService,
-    private readonly jwtService: JwtService
+    private readonly jwtService: JwtService,
   ) {}
 
   @HttpCode(200)
   @Post('/login')
   async login(
-    @Body() signinCredential: SignInCredentialsDto
+    @Body() signinCredential: SignInCredentialsDto,
   ): Promise<{ token: string }> {
     const user = await this.authServie.login({
       email: signinCredential.email,
@@ -31,7 +31,7 @@ export class AuthController {
     // validation for password
     const answe = await bcrypt.compare(
       signinCredential.password,
-      user?.password || ''
+      user?.password || '',
     );
 
     if (!answe || !user) throw new UnauthorizedException('Invalid Credentials');
